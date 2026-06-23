ea8fcdcbdda067941b4069cdbe1d6cb9c4608c0d
Mriganshu Kumar Singh <mriganshukumarsingh@gmail.com>
2026-06-23 17:03:44 +0530

    Fix unused imports

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -1,13 +1,8 @@
 import {
-  FaGithub,
-  FaInstagram,
   FaLinkedinIn,
-  FaYoutube,
 } from "react-icons/fa6";
 import "./styles/SocialIcons.css";
-import { TbNotes } from "react-icons/tb";
 import { useEffect } from "react";
-import HoverLinks from "./HoverLinks";
 
 const SocialIcons = () => {
   useEffect(() => {
